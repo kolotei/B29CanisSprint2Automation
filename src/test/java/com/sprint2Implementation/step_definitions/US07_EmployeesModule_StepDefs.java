e2036e0d05d88367c3354b1c57a42aa3dbf9b227
odilj <odiljono@outlook.com>
2023-07-25 07:54:58 -0400

    Renamed Emp Page and Emp Step Def

--- src/test/java/com/sprint2Implementation/step_definitions/US07_EmployeesModule_StepDefs.java
+++ src/test/java/com/sprint2Implementation/step_definitions/US07_EmployeesModule_StepDefs.java
@@ -1,7 +1,6 @@
 package com.sprint2Implementation.step_definitions;
 
-import com.sprint2Implementation.pages.EmployeePage;
-import com.sprint2Implementation.utilities.BrowserUtils;
+import com.sprint2Implementation.pages.EmployeesPage;
 import com.sprint2Implementation.utilities.Driver;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -12,13 +11,13 @@ import org.openqa.selenium.WebElement;
 import java.util.ArrayList;
 import java.util.List;
 
-public class US07_EmployeeModule_StepDefs {
-    EmployeePage employeePage = new EmployeePage();
+public class US07_EmployeesModule_StepDefs {
+    EmployeesPage employeesPage = new EmployeesPage();
 
     @When("user clicks on Employee module")
     public void user_clicks_on_employee_module() {
 
-        employeePage.employeeBtn.click();
+        employeesPage.employeesBtn.click();
     }
 
     @Then("user is able to see expected modules on the Employee page")
@@ -27,17 +26,17 @@ public class US07_EmployeeModule_StepDefs {
         List<String> actualModules = new ArrayList<>();
         Driver.getDriver().manage().window().setSize(new Dimension(1920, 1080));
 
-        for (WebElement eachModule : employeePage.employeePageModules) {
+        for (WebElement eachModule : employeesPage.employeesPageModules) {
             actualModules.add(eachModule.getText());
         }
-        System.out.println(actualModules);
+
         Assert.assertEquals(expectedModules, actualModules);
     }
 
     @Then("user is able to see Company Structure by default")
     public void user_is_able_to_see_company_structure_by_default() {
 
-        Assert.assertEquals("Company Structure", employeePage.module1.getText());
+        Assert.assertEquals("Company Structure", employeesPage.module1.getText());
 
     }
 }
